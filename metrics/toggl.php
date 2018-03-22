05014c8d964ef2e2fd1641fa2232685c1e3a2230
gmarraffa <marraffa3ai@gmail.com>
2018-03-22 18:23:12 +0100

    Classificazione ruolo e descrizione in script toggl

--- metrics/toggl.php
+++ metrics/toggl.php
@@ -3,6 +3,17 @@ require_once("../connection.php");
 $data = $_GET;
 
 $data["at"] = date('Y-m-d H:i:s');
+preg_match_all('/\[[a-z\-0-9]+\]/i', $data['description'], $matches);
+$found = $matches[0];
+if(sizeof($found) > 0){
+  $data["role"] = str_replace('[', '', $found[0]);
+  $data["role"] = str_replace(']', '', $data['role']);
+}
+if(sizeof($found) > 1){
+  $data["detail"] = str_replace('[', '', $found[1]);
+  $data["detail"] = str_replace(']', '', $data['detail']);
+}
+
 
 var_dump($data);
 DB::insert('toggl', $data);
